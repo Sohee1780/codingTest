ade5039932ee92ab70eee0c1ce644d11d81940f8
Sohee1780 <128003056+Sohee1780@users.noreply.github.com>
2023-06-21 17:45:03 +0900

    [Bronze I] Title: 단어 공부, Time: 556 ms, Memory: 33824 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -5,23 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
-		
-		char[] cArr = new char[26];
-		for(int i=0; i<26; i++) {
-			cArr[i]=(char)(97+i);
-		}
-		
-		int[] count = new int[26];
-		
 		String str = s.nextLine().toLowerCase();
 		char[] cStr = str.toCharArray();
 		
+		int[] count = new int[26];
+		
 		for(int i=0; i<cStr.length; i++) {
-			for(int j=0; j<26; j++) {
-				if(cStr[i]==cArr[j]) {
-					count[j]+=1;
-				}
-			}
+			count[cStr[i]-97] +=1; 
 		}
 		
 		int max=0;
@@ -30,18 +20,10 @@ public class Main {
 		for(int i=0; i<26; i++) {
 			if(max<count[i]) {
 				max = count[i];
-				t=String.valueOf(cArr[i]).toUpperCase();
+				t= String.valueOf((char)(i+97)).toUpperCase();
+			}else if(max==count[i]){
+				t="?";
 			}
-		}
-
-		int maxC=0;
-		for(int i=0; i<26; i++) {
-			if(max==count[i]) {
-				maxC+=1;
-			}
-		}
-		if(maxC>1) {
-			t="?";
 		}
 		System.out.println(t);
 	}
